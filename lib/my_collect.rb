3a228db59312bcd701947b4fb8d40215e748726c
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-07 17:04:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,15 @@ def my_collect(array)
     return array
   end
   i=0
+<<<<<<< HEAD
   new_array=[]
   while i<=array.size-1
    new_array.push(yield array[i])
+=======
+  new_array=array
+  while i<=array.size-1
+   new_array[i] = yield array[i]
+>>>>>>> cd2147e97fabee6dd8dca64f92b35f63fb154168
     i+=1
   end
   new_array
